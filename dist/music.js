75c26a36556c30d514c8199132c30bab584ea7a3
zwardtooommy <870553699@qq.com>
2019-02-13 18:04:47 +0800

    Site updated: 2019-02-13 18:04:40

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "We Don't Talk Anymore",
+        artist: 'Charlie Puth',
+        url: 'https////music.163.com/outchain/player?type=2&id=1332052297&auto=1&height=66',
+      }
+    ]
+});
